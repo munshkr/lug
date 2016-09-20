4b4735c5d70930ff0b61ae3f47106d50183df91f
Damián Silvani <munshkr@gmail.com>
2016-09-20 00:29:31 -0300

    Set constant name to dynamic modules created by #logger_on

--- lib/mang/extend.rb
+++ lib/mang/extend.rb
@@ -1,6 +1,9 @@
 require 'mang'
 
 def logger_on(namespace, logger_method = :logger)
+  mod_name = :"Log_#{namespace}"
+  return Mang.const_get(mod_name) if Mang.const_defined?(mod_name)
+
   mod = Module.new
   mod.module_eval(%(
     module ClassMethods
@@ -17,5 +20,6 @@ def logger_on(namespace, logger_method = :logger)
       self.class.#{logger_method}
     end
   ))
+  Mang.const_set(:"Log_#{namespace}", mod)
   mod
 end
