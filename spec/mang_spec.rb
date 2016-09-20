9f4b2b78c9e49ffd48863a62c9eaa4df8714551a
Damián Silvani <munshkr@gmail.com>
2016-09-20 07:26:09 -0300

    Add more specs

--- spec/mang_spec.rb
+++ spec/mang_spec.rb
@@ -6,95 +6,156 @@ describe Mang do
     refute_nil ::Mang::VERSION
   end
 
-  describe 'when device is not a TTY' do
-    before do
-      @io = StringIO.new
+  describe '#initialize' do
+    it 'defaults to no namespace and STDERR io' do
+      logger = Mang::Logger.new
+      assert_nil logger.namespace
+      assert_equal STDERR, logger.io
     end
+  end
 
-    describe '#log without namespace' do
+  describe '#on' do
+    describe 'without default namespace' do
       before do
+        @io = StringIO.new
         @logger = Mang::Logger.new(nil, @io)
       end
 
-      it 'logs message' do
-        Timecop.freeze(Time.now) do
-          @logger.log('my message')
-          assert_equal "#{Time.now} my message\n", @io.string
+      it 'clones Logger with namespace appended to default' do
+        logger = @logger.on(:script)
+
+        assert_instance_of Mang::Logger, logger
+        assert_equal @logger.io.object_id, logger.io.object_id
+        assert_equal 'script', logger.namespace
+      end
+    end
+
+    describe 'with default namespace' do
+      before do
+        @io = StringIO.new
+        @logger = Mang::Logger.new(:main, @io)
+      end
+
+      it 'clones Logger with namespace' do
+        logger = @logger.on(:script)
+
+        assert_instance_of Mang::Logger, logger
+        assert_equal @logger.io.object_id, logger.io.object_id
+        assert_equal 'main:script', logger.namespace
+      end
+    end
+  end
+
+  describe '#log' do
+    describe 'when device is not a TTY' do
+      before do
+        @io = StringIO.new
+      end
+
+      describe 'without namespace' do
+        before do
+          @logger = Mang::Logger.new(nil, @io)
+        end
+
+        it 'logs message' do
+          Timecop.freeze(Time.now) do
+            @logger.log('my message')
+            assert_equal "#{Time.now} my message\n", @io.string
+          end
+        end
+
+        it 'logs message from block' do
+          Timecop.freeze(Time.now) do
+            @logger.log { 'my message' }
+            assert_equal "#{Time.now} my message\n", @io.string
+          end
         end
       end
 
-      it 'logs message from block' do
-        Timecop.freeze(Time.now) do
-          @logger.log { 'my message' }
-          assert_equal "#{Time.now} my message\n", @io.string
+      describe 'with default namespace' do
+        before do
+          @logger = Mang::Logger.new(:main, @io)
+        end
+
+        it 'logs message with default namespace' do
+          Timecop.freeze(Time.now) do
+            @logger.log('my message')
+            assert_equal "#{Time.now} [main] my message\n", @io.string
+          end
+        end
+
+        it 'logs message from block with default namespace' do
+          Timecop.freeze(Time.now) do
+            @logger.log { 'my message' }
+            assert_equal "#{Time.now} [main] my message\n", @io.string
+          end
         end
       end
     end
 
-    describe '#log with default namespace' do
-      before do
-        @logger = Mang::Logger.new(:main, @io)
-      end
-
-      it 'logs message with default namespace' do
-        Timecop.freeze(Time.now) do
-          @logger.log('my message')
-          assert_equal "#{Time.now} [main] my message\n", @io.string
+    describe 'when device is a TTY' do
+      def line_re(ns, msg)
+        if ns
+          /\e\[\d+;\d+m#{ns}\e\[0m \e\[\d+;\d+m#{msg}\e\[0m \+\d+[ms]+/
+        else
+          /\e\[\d+;\d+m#{msg}\e\[0m \+\d+[ms]+/
         end
       end
 
-      it 'logs message from block with default namespace' do
-        Timecop.freeze(Time.now) do
+      before do
+        @io = TtyMockIO.new
+      end
+
+      describe 'without namespace' do
+        before do
+          @logger = Mang::Logger.new(nil, @io)
+        end
+
+        it 'logs message' do
+          @logger.log('my message')
+          assert_match line_re(nil, 'my message'), @io.string
+        end
+
+        it 'logs message from block' do
           @logger.log { 'my message' }
-          assert_equal "#{Time.now} [main] my message\n", @io.string
+          assert_match line_re(nil, 'my message'), @io.string
+        end
+      end
+
+      describe 'with default namespace' do
+        before do
+          @logger = Mang::Logger.new(:main, @io)
+        end
+
+        it 'logs message with default namespace' do
+          @logger.log('my message')
+          assert_match line_re('main', 'my message'), @io.string
+        end
+
+        it 'logs message from block with default namespace' do
+          @logger.log { 'my message' }
+          assert_match line_re('main', 'my message'), @io.string
         end
       end
     end
   end
 
-  describe 'when device is a TTY' do
-    def line_re(ns, msg)
-      if ns
-        /\e\[\d+;\d+m#{ns}\e\[0m \e\[\d+;\d+m#{msg}\e\[0m \+\d+[ms]+/
-      else
-        /\e\[\d+;\d+m#{msg}\e\[0m \+\d+[ms]+/
-      end
-    end
-
+  describe '#<<' do
     before do
-      @io = TtyMockIO.new
+      @io = StringIO.new
+      @logger = Mang::Logger.new(nil, @io)
     end
 
-    describe '#log without namespace' do
-      before do
-        @logger = Mang::Logger.new(nil, @io)
-      end
+    it 'is an alias of #log' do
+      @logger.log 'message'
+      res_log = @io.string
 
-      it 'logs message' do
-        @logger.log('my message')
-        assert_match line_re(nil, 'my message'), @io.string
-      end
+      @io.truncate(0)
 
-      it 'logs message from block' do
-        @logger.log { 'my message' }
-        assert_match line_re(nil, 'my message'), @io.string
-      end
-    end
+      @logger << 'message'
+      res = @io.string
 
-    describe '#log with default namespace' do
-      before do
-        @logger = Mang::Logger.new(:main, @io)
-      end
-
-      it 'logs message with default namespace' do
-        @logger.log('my message')
-        assert_match line_re('main', 'my message'), @io.string
-      end
-
-      it 'logs message from block with default namespace' do
-        @logger.log { 'my message' }
-        assert_match line_re('main', 'my message'), @io.string
-      end
+      assert_equal res_log, res
     end
   end
 
