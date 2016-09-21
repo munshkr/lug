945f2f3034c76091c975c2ef78ad0ebe15e077cc
Damián Silvani <munshkr@gmail.com>
2016-09-21 00:12:20 -0300

    Delegate arguments on Lug.create

--- lib/lug.rb
+++ lib/lug.rb
@@ -6,7 +6,7 @@ require 'lug/tty_logger'
 # Small utility class for logging messages to stderr or other IO device
 #
 module Lug
-  def self.create(namespace = nil, io = STDERR)
-    io.isatty ? TtyLogger.new(namespace, io) : Logger.new(namespace, io)
+  def self.create(*args)
+    io.isatty ? TtyLogger.new(*args) : Logger.new(*args)
   end
 end
